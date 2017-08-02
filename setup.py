b2518f8ac8bfa8a7858a34dc78f1bf6df4f52ef7
Dariusz Suchojad <dsuch-github@m.zato.io>
2017-08-02 18:25:00 +0200

    Version set to 1.6.

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ from __future__ import absolute_import, division, print_function, unicode_litera
 import os, sys
 from setuptools import setup, find_packages
 
-version = '1.5'
+version = '1.6'
 
 LONG_DESCRIPTION = """"""
 
